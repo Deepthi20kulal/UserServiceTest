a1d65f7db1fbd5bca6399c351aa1ac610372a6b2
deepthi20kulal <20deepthikulal@gmail.com>
2025-09-24 10:24:03 +0530

    1st

---
+++ src/main/java/com/example/entity/UserEntityDeepthi.java
@@ -0,0 +1,14 @@
+package com.example.entity;
+
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Data
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="user")
+public class UserEntityDeepthi {
+
+}
